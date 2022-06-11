0f11b4feb21a40d189050dc632dc8efa9a9296cf
Eduardo Henckemaier <eduhenckemaier@outlook.com>
2022-06-11 20:05:21 -0300

    .

---
+++ ControleMedicamentos.Infra.BancoDados/ModuloFuncionario/RepositorioFuncionarioEmBancoDados.cs
@@ -0,0 +1,154 @@
+﻿
+
+using ControleMedicamentos.Dominio.ModuloFuncionario;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace ControleMedicamentos.Infra.BancoDados.ModuloFuncionario
+{
+    public class RepositorioFuncionarioEmBancoDados : IRepositorioFuncionario
+    {
+        private const string enderecoBanco =
+               "Data Source=(LocalDB)\\MSSqlLocalDB;" +
+               "Initial Catalog=ControleMedimentoDb;" +
+               "Integrated Security=True;" +
+               "Pooling=False";
+
+        #region Sql Queries
+        private const string sqlInserir =
+            @"INSERT INTO [TBFUNCIONARIO] 
+                (
+                    [NOME],
+                    [LOGIN],
+                    [SENHA]
+	            )
+	            VALUES
+                (
+                    @NOME,
+                    @LOGIN,
+                    @SENHA
+                );SELECT SCOPE_IDENTITY();";
+
+        private const string sqlEditar =
+            @"UPDATE [TBFUNCIONARIO]	
+		        SET
+			        [NOME] = @NOME,
+			        [LOGIN] = @LOGIN,
+                    [SENHA] = @SENHA
+		        WHERE
+			        [ID] = @ID";
+
+        private const string sqlExcluir =
+            @"DELETE FROM [TBFUNCIONARIO]
+		        WHERE
+			        [ID] = @ID";
+
+        private const string sqlSelecionarTodos =
+            @"SELECT 
+		            [ID], 
+		            [NOME], 
+		            [LOGIN],
+                    [SENHA]
+	            FROM 
+		            [TBFUNCIONARIO]";
+
+        private const string sqlSelecionarPorId =
+            @"SELECT 
+		            [ID], 
+		            [NOME], 
+		            [LOGIN],
+                    [SENHA]
+	            FROM 
+		            [TBFUNCIONARIO]
+		        WHERE
+                    [ID] = @ID";
+        #endregion
+
+        public ValidationResult Inserir(Funcionario novoRegistro)
+        {
+            var validador = new ValidadorFuncionario();
+
+            var resultadoValidacao = validador.Validate(novoRegistro);
+
+            if (resultadoValidacao.IsValid == false)
+                return resultadoValidacao;
+
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+
+            SqlCommand comandoInsercao = new SqlCommand(sqlInserir, conexaoComBanco);
+
+            ConfigurarParametrosFuncionario(novoRegistro, comandoInsercao);
+
+            conexaoComBanco.Open();
+            var id = comandoInsercao.ExecuteScalar();
+            novoRegistro.Id = Convert.ToInt32(id);
+
+            conexaoComBanco.Close();
+
+            return resultadoValidacao;
+        }
+
+        public ValidationResult Editar(Funcionario registro)
+        {
+            var validador = new ValidadorFuncionario();
+
+            var resultadoValidacao = validador.Validate(registro);
+
+            if (resultadoValidacao.IsValid == false)
+                return resultadoValidacao;
+
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+
+            SqlCommand comandoEdicao = new SqlCommand(sqlEditar, conexaoComBanco);
+
+            ConfigurarParametrosFuncionario(registro, comandoEdicao);
+
+            conexaoComBanco.Open();
+            comandoEdicao.ExecuteNonQuery();
+            conexaoComBanco.Close();
+
+            return resultadoValidacao;
+        }
+
+        public ValidationResult Excluir(Funcionario registro)
+        {
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+
+            SqlCommand comandoExclusao = new SqlCommand(sqlExcluir, conexaoComBanco);
+
+            comandoExclusao.Parameters.AddWithValue("ID", registro.Id);
+
+            conexaoComBanco.Open();
+            int numeroRegistrosExcluidos = comandoExclusao.ExecuteNonQuery();
+
+            var resultadoValidacao = new ValidationResult();
+
+            if (numeroRegistrosExcluidos == 0)
+                resultadoValidacao.Errors.Add(new ValidationFailure("", "Não foi possível remover o funcionario :("));
+
+            conexaoComBanco.Close();
+
+            return resultadoValidacao;
+        }
+
+
+        public Funcionario SelecionarPorNumero(int numero)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Funcionario> SelecionarTodos()
+        {
+            throw new System.NotImplementedException();
+        }
+        private void ConfigurarParametrosFuncionario(Funcionario novoRegistro, SqlCommand comando)
+        {
+            comando.Parameters.AddWithValue("ID", novoRegistro.Id);
+            comando.Parameters.AddWithValue("NOME", novoRegistro.Nome);
+            comando.Parameters.AddWithValue("LOGIN", novoRegistro.Login);
+            comando.Parameters.AddWithValue("SENHA", novoRegistro.Senha);
+        }
+    }
+}
