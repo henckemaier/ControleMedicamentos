e1ad078f8e014d5fa7bdd5c48188fccc817c8057
Eduardo Henckemaier <eduhenckemaier@outlook.com>
2022-06-14 16:20:34 -0300

    .

--- ControleMedicamentos.Dominio.Tests/ModuloMedicamento/ValidadorMedicamentoTest.cs
+++ ControleMedicamentos.Dominio.Tests/ModuloMedicamento/ValidadorMedicamentoTest.cs
@@ -1,5 +1,6 @@
 ﻿using ControleMedicamentos.Dominio.ModuloMedicamento;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Globalization;
 
 namespace ControleMedicamentos.Dominio.Tests.ModuloMedicamento
@@ -28,5 +29,58 @@ namespace ControleMedicamentos.Dominio.Tests.ModuloMedicamento
             Assert.AreEqual("'Nome' não pode ser nulo.", resultado.Errors[0].ErrorMessage);
         }
 
+        [TestMethod]
+        public void Descricao_do_medicamento_deve_ser_obrigatorio()
+        {
+            //arrange
+            var m = new Medicamento();
+            m.Nome = "Decongeste";
+            m.Descricao = null;
+
+            ValidadorMedicamento validador = new ValidadorMedicamento();
+
+            //action
+            var resultado = validador.Validate(m);
+
+            //assert
+            Assert.AreEqual("'Descricao' não pode ser nulo.", resultado.Errors[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Lote_do_medicamento_deve_ser_obrigatorio()
+        {
+            //arrange
+            var m = new Medicamento();
+            m.Nome = "Decongeste";
+            m.Descricao = "Alivia a febre";
+            m.Lote = null;
+
+            ValidadorMedicamento validador = new ValidadorMedicamento();
+
+            //action
+            var resultado = validador.Validate(m);
+
+            //assert
+            Assert.AreEqual("'Lote' não pode ser nulo.", resultado.Errors[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Validade_do_medicamento_deve_ser_obrigatorio()
+        {
+            //arrange
+            var m = new Medicamento();
+            m.Nome = "Decongeste";
+            m.Descricao = "Alivia a febre";
+            m.Lote = "123";
+            m.Validade = DateTime.MinValue;
+
+            ValidadorMedicamento validador = new ValidadorMedicamento();
+
+            //action
+            var resultado = validador.Validate(m);
+
+            //assert
+            Assert.AreEqual("'Validade' deve ser informado.", resultado.Errors[0].ErrorMessage);
+        }
     }
 }
--- ControleMedicamentos.Infra.BancoDados.Tests/ModuloMedicamento/RepositorioMedicamentoEmBancoDadosTest.cs
+++ ControleMedicamentos.Infra.BancoDados.Tests/ModuloMedicamento/RepositorioMedicamentoEmBancoDadosTest.cs
@@ -1,3 +1,7 @@
+using ControleMedicamento.Infra.BancoDados.ModuloMedicamento;
+using ControleMedicamentos.Dominio.ModuloMedicamento;
+using ControleMedicamentos.Infra.BancoDados.Compartilhado;
+using ControleMedicamentos.Infra.BancoDados.ModuloFornecedor;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ControleMedicamento.Infra.BancoDados.Tests.ModuloMedicamento
@@ -5,6 +9,22 @@ namespace ControleMedicamento.Infra.BancoDados.Tests.ModuloMedicamento
     [TestClass]
     public class RepositorioMedicamentoEmBancoDadosTest
     {
+        private readonly Medicamento medicamento;
+        private readonly RepositorioMedicamentoEmBancoDados repositorioMedicamento;
+        private readonly RepositorioFornecedorEmBancoDados repositorioFornecedor;
+
+        public RepositorioMedicamentoEmBancoDadosTest()
+        {
+            string sql =
+                @"DELETE FROM TBMEDICAMENTO;
+                  DBCC CHECKIDENT (TBMEDICAMENTO, RESEED, 0)
+
+                 DELETE FROM TBFORNECEDOR;
+                  DBCC CHECKIDENT (TBFORNECEDOR, RESEED, 0)";
+
+            Db.ExecutarSql(sql);
+        }
+
         [TestMethod]
         public void Deve_inserir_medicamento()
         {
