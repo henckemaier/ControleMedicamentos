b2e0fb05393bfe2c976ecef3e641a4d189b89e07
Eduardo Henckemaier <eduhenckemaier@outlook.com>
2022-06-14 15:32:36 -0300

    .

---
+++ ControleMedicamentos.Dominio.Tests/ModuloMedicamento/ValidadorMedicamentoTest.cs
@@ -0,0 +1,32 @@
+﻿using ControleMedicamentos.Dominio.ModuloMedicamento;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+
+namespace ControleMedicamentos.Dominio.Tests.ModuloMedicamento
+{
+    [TestClass]
+    public class ValidadorMedicamentoTest
+    {
+        public ValidadorMedicamentoTest()
+        {
+            CultureInfo.CurrentUICulture = new CultureInfo("pt-BR");
+        }
+
+        [TestMethod]
+        public void Nome_do_medicamento_deve_ser_obrigatorio()
+        {
+            //arrange
+            var m = new Medicamento();
+            m.Nome = null;
+
+            ValidadorMedicamento validador = new ValidadorMedicamento();
+
+            //action
+            var resultado = validador.Validate(m);
+
+            //assert
+            Assert.AreEqual("'Nome' não pode ser nulo.", resultado.Errors[0].ErrorMessage);
+        }
+
+    }
+}
