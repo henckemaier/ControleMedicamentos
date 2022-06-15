2b4bae80bb28ee54664cb39ed1bd9b5e6c446ec5
Eduardo Henckemaier <eduhenckemaier@outlook.com>
2022-06-15 18:43:55 -0300

    .

--- ControleMedicamentos.Infra.BancoDados.Tests/ModuloRequisicao/RepositorioRequisicaoEmBancoDadosTest.cs
+++ ControleMedicamentos.Infra.BancoDados.Tests/ModuloRequisicao/RepositorioRequisicaoEmBancoDadosTest.cs
@@ -1,10 +1,20 @@
-﻿using ControleMedicamentos.Dominio.ModuloFuncionario;
+﻿using ControleMedicamento.Infra.BancoDados.ModuloMedicamento;
+using ControleMedicamentos.Dominio.ModuloFornecedor;
+using ControleMedicamentos.Dominio.ModuloFuncionario;
+using ControleMedicamentos.Dominio.ModuloMedicamento;
+using ControleMedicamentos.Dominio.ModuloPaciente;
+using ControleMedicamentos.Dominio.ModuloRequisicao;
 using ControleMedicamentos.Infra.BancoDados.Compartilhado;
+using ControleMedicamentos.Infra.BancoDados.ModuloFornecedor;
 using ControleMedicamentos.Infra.BancoDados.ModuloFuncionario;
+using ControleMedicamentos.Infra.BancoDados.ModuloPaciente;
+using ControleMedicamentos.Infra.BancoDados.ModuloRequisicao;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace ControleMedicamentos.Infra.BancoDados.Tests.ModuloRequisicao
 {
+    [TestClass]
     public class RepositorioRequisicaoEmBancoDadosTest
     {
         public RepositorioRequisicaoEmBancoDadosTest()
@@ -42,33 +52,321 @@ namespace ControleMedicamentos.Infra.BancoDados.Tests.ModuloRequisicao
 
             repositorioFuncionario.Inserir(novoFuncionario);
 
-            Medicamento novoMedicamento = new()
+            /////////////////////////////////
+
+            Paciente novoPaciente = new()
             {
-                Nome = "Decongeste",
-                Descricao = "Bom para febre",
-                Lote = "123",
-                Validade = DateTime.Now.AddDays(30).Date,
-                QuantidadeDisponivel = 12,
-                Fornecedor = novoFuncionario
+                Nome = "Lucas",
+                CartaoSUS = "123456789012345"
             };
 
-            var repositorio = new RepositorioMedicamentoEmBancoDados();
+            var repositorioPaciente = new RepositorioPacienteEmBancoDeDados();
 
-            //action
-            repositorio.Inserir(novoMedicamento);
+            repositorioPaciente.Inserir(novoPaciente);
+
+            /////////////////////////////////
+
+            Fornecedor novoFornecedor = new()
+            {
+                Nome = "Lucas",
+                Telefone = "11111111111",
+                Email = "blabla@gmail.com",
+                Cidade = "Lages",
+                Estado = "Santa Catarina"
+            };
+
+            var repositorioFornecedor = new RepositorioFornecedorEmBancoDados();
+
+            repositorioFornecedor.Inserir(novoFornecedor);
+
+            ////////////////////////////////
+
+            Medicamento novoMedicamento = new()
+            {
+                Nome = "Lucas",
+                Descricao = "asdagvfgdgdfgdf",
+                Lote = "12345",
+                Validade = DateTime.Now.AddDays(30).Date,
+                QuantidadeDisponivel = 10,
+                Fornecedor = novoFornecedor
+            };
+
+            var repositorioMedicamento = new RepositorioMedicamentoEmBancoDados();
+
+            repositorioMedicamento.Inserir(novoMedicamento);
+
+            ////////////////////////////////
+
+            Requisicao novoRequisicao = new()
+            {
+                Funcionario = novoFuncionario,
+                Paciente = novoPaciente,
+                Medicamento = novoMedicamento,
+                QtdMedicamento = 5,
+                Data = DateTime.Now.Date
+            };
+
+            var repositorioRequisicao = new RepositorioRequisicaoEmBancoDados();
+
+            repositorioRequisicao.Inserir(novoRequisicao);
 
             //assert
-            Medicamento medicamentoEncontrado = repositorio.SelecionarPorNumero(novoMedicamento.Id);
+            Requisicao requisicaoEncontrada = repositorioRequisicao.SelecionarPorNumero(novoRequisicao.Id);
 
-            Assert.IsNotNull(medicamentoEncontrado);
-            Assert.AreEqual(novoMedicamento.Id, medicamentoEncontrado.Id);
-            Assert.AreEqual(novoMedicamento.Nome, medicamentoEncontrado.Nome);
-            Assert.AreEqual(novoMedicamento.Descricao, medicamentoEncontrado.Descricao);
-            Assert.AreEqual(novoMedicamento.Lote, medicamentoEncontrado.Lote);
-            Assert.AreEqual(novoMedicamento.Validade, medicamentoEncontrado.Validade);
-            Assert.AreEqual(novoMedicamento.QuantidadeDisponivel, medicamentoEncontrado.QuantidadeDisponivel);
-            Assert.AreEqual(novoMedicamento.Fornecedor.Id, medicamentoEncontrado.Fornecedor.Id);
+            Assert.IsNotNull(requisicaoEncontrada);
+            Assert.AreEqual(novoRequisicao.Id, requisicaoEncontrada.Id);
+            Assert.AreEqual(novoRequisicao.Funcionario.Id, requisicaoEncontrada.Funcionario.Id);
+            Assert.AreEqual(novoRequisicao.Paciente.Id, requisicaoEncontrada.Paciente.Id);
+            Assert.AreEqual(novoRequisicao.Medicamento.Id, requisicaoEncontrada.Medicamento.Id);
+            Assert.AreEqual(novoRequisicao.QtdMedicamento, requisicaoEncontrada.QtdMedicamento);
+            Assert.AreEqual(novoRequisicao.Data, requisicaoEncontrada.Data);
         }
-    }
+
+        [TestMethod]
+        public void Deve_editar_requisicao()
+        {
+            Funcionario novoFuncionario = new()
+            {
+                Nome = "Edu",
+                Login = "edu2345",
+                Senha = "1234567"
+            };
+
+            var repositorioFuncionario = new RepositorioFuncionarioEmBancoDados();
+
+            repositorioFuncionario.Inserir(novoFuncionario);
+
+            /////////////////////////////////
+
+            Paciente novoPaciente = new()
+            {
+                Nome = "Lucas",
+                CartaoSUS = "123456789012345"
+            };
+
+            var repositorioPaciente = new RepositorioPacienteEmBancoDeDados();
+
+            repositorioPaciente.Inserir(novoPaciente);
+
+            /////////////////////////////////
+
+            Fornecedor novoFornecedor = new()
+            {
+                Nome = "Lucas",
+                Telefone = "11111111111",
+                Email = "blabla@gmail.com",
+                Cidade = "Lages",
+                Estado = "Santa Catarina"
+            };
+
+            var repositorioFornecedor = new RepositorioFornecedorEmBancoDados();
+
+            repositorioFornecedor.Inserir(novoFornecedor);
+
+            ////////////////////////////////
+
+            Medicamento novoMedicamento = new()
+            {
+                Nome = "Lucas",
+                Descricao = "asdagvfgdgdfgdf",
+                Lote = "12345",
+                Validade = DateTime.Now.AddDays(30).Date,
+                QuantidadeDisponivel = 10,
+                Fornecedor = novoFornecedor
+            };
+
+            var repositorioMedicamento = new RepositorioMedicamentoEmBancoDados();
+
+            repositorioMedicamento.Inserir(novoMedicamento);
+
+            ////////////////////////////////
+
+            Requisicao novoRequisicao = new()
+            {
+                Funcionario = novoFuncionario,
+                Paciente = novoPaciente,
+                Medicamento = novoMedicamento,
+                QtdMedicamento = 5,
+                Data = DateTime.Now.Date
+            };
+
+            var repositorioRequisicao = new RepositorioRequisicaoEmBancoDados();
+
+            repositorioRequisicao.Inserir(novoRequisicao);
+
+            Requisicao requisicaoAtualizada = repositorioRequisicao.SelecionarPorNumero(novoMedicamento.Id);
+            requisicaoAtualizada.Funcionario = novoFuncionario;
+            requisicaoAtualizada.Paciente = novoPaciente;
+            requisicaoAtualizada.Medicamento = novoMedicamento;
+            requisicaoAtualizada.QtdMedicamento = 5;
+            requisicaoAtualizada.Data = DateTime.Now.Date;
+
+            //action
+            repositorioRequisicao.Editar(requisicaoAtualizada);
+
+            //assert
+            Requisicao requisicaoEncontrada = repositorioRequisicao.SelecionarPorNumero(novoRequisicao.Id);
+
+            Assert.IsNotNull(requisicaoEncontrada);
+            Assert.AreEqual(novoRequisicao.Id, requisicaoEncontrada.Id);
+            Assert.AreEqual(novoRequisicao.Funcionario.Id, requisicaoEncontrada.Funcionario.Id);
+            Assert.AreEqual(novoRequisicao.Paciente.Id, requisicaoEncontrada.Paciente.Id);
+            Assert.AreEqual(novoRequisicao.Medicamento.Id, requisicaoEncontrada.Medicamento.Id);
+            Assert.AreEqual(novoRequisicao.QtdMedicamento, requisicaoEncontrada.QtdMedicamento);
+            Assert.AreEqual(novoRequisicao.Data, requisicaoEncontrada.Data);
+        }
+
+        [TestMethod]
+        public void Deve_excluir_requisicao()
+        {
+            Funcionario novoFuncionario = new()
+            {
+                Nome = "Edu",
+                Login = "edu2345",
+                Senha = "1234567"
+            };
+
+            var repositorioFuncionario = new RepositorioFuncionarioEmBancoDados();
+
+            repositorioFuncionario.Inserir(novoFuncionario);
+
+            /////////////////////////////////
+
+            Paciente novoPaciente = new()
+            {
+                Nome = "Lucas",
+                CartaoSUS = "123456789012345"
+            };
+
+            var repositorioPaciente = new RepositorioPacienteEmBancoDeDados();
+
+            repositorioPaciente.Inserir(novoPaciente);
+
+            /////////////////////////////////
+
+            Fornecedor novoFornecedor = new()
+            {
+                Nome = "Lucas",
+                Telefone = "11111111111",
+                Email = "blabla@gmail.com",
+                Cidade = "Lages",
+                Estado = "Santa Catarina"
+            };
+
+            var repositorioFornecedor = new RepositorioFornecedorEmBancoDados();
+
+            repositorioFornecedor.Inserir(novoFornecedor);
+
+            ////////////////////////////////
+
+            Medicamento novoMedicamento = new()
+            {
+                Nome = "Lucas",
+                Descricao = "asdagvfgdgdfgdf",
+                Lote = "12345",
+                Validade = DateTime.Now.AddDays(30).Date,
+                QuantidadeDisponivel = 10,
+                Fornecedor = novoFornecedor
+            };
+
+            var repositorioMedicamento = new RepositorioMedicamentoEmBancoDados();
+
+            repositorioMedicamento.Inserir(novoMedicamento);
+
+            ////////////////////////////////
+
+            Requisicao novoRequisicao1 = new()
+            {
+                Funcionario = novoFuncionario,
+                Paciente = novoPaciente,
+                Medicamento = novoMedicamento,
+                QtdMedicamento = 5,
+                Data = DateTime.Now.Date
+            };
+
+            var repositorioRequisicao = new RepositorioRequisicaoEmBancoDados();
+
+            repositorioRequisicao.Inserir(novoRequisicao1);
+
+            
+
+            //action
+            repositorioRequisicao.Excluir(requisicaoEncontrada);
+
+            //assert
+            Assert.IsNotNull(requisicaoEncontrada);
+        }
+        [TestMethod]
+        public void Deve_selecionar_todas_requisicoes()
+        {
+            Funcionario novoFuncionario = new()
+            {
+                Nome = "Edu",
+                Login = "edu2345",
+                Senha = "1234567"
+            };
+
+            var repositorioFuncionario = new RepositorioFuncionarioEmBancoDados();
+
+            repositorioFuncionario.Inserir(novoFuncionario);
+
+            /////////////////////////////////
+
+            Paciente novoPaciente = new()
+            {
+                Nome = "Lucas",
+                CartaoSUS = "123456789012345"
+            };
+
+            var repositorioPaciente = new RepositorioPacienteEmBancoDeDados();
+
+            repositorioPaciente.Inserir(novoPaciente);
+
+            /////////////////////////////////
+
+            Fornecedor novoFornecedor = new()
+            {
+                Nome = "Lucas",
+                Telefone = "11111111111",
+                Email = "blabla@gmail.com",
+                Cidade = "Lages",
+                Estado = "Santa Catarina"
+            };
+
+            var repositorioFornecedor = new RepositorioFornecedorEmBancoDados();
+
+            repositorioFornecedor.Inserir(novoFornecedor);
+
+            ////////////////////////////////
+
+            Medicamento novoMedicamento = new()
+            {
+                Nome = "Lucas",
+                Descricao = "asdagvfgdgdfgdf",
+                Lote = "12345",
+                Validade = DateTime.Now.AddDays(30).Date,
+                QuantidadeDisponivel = 10,
+                Fornecedor = novoFornecedor
+            };
+
+            var repositorioMedicamento = new RepositorioMedicamentoEmBancoDados();
+
+            repositorioMedicamento.Inserir(novoMedicamento);
+
+            ////////////////////////////////
+
+            Requisicao novoRequisicao = new()
+            {
+                Funcionario = novoFuncionario,
+                Paciente = novoPaciente,
+                Medicamento = novoMedicamento,
+                QtdMedicamento = 5,
+                Data = DateTime.Now.Date
+            };
+
+            var repositorioRequisicao = new RepositorioRequisicaoEmBancoDados();
+
+            repositorioRequisicao.Inserir(novoRequisicao);
+        }
     }
 }
